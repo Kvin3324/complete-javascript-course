6c45157619f97237f65c72c8fb09a8dad1195c72
kevin joya <kevin.joya@outlook.com>
2018-12-17 15:33:47 +0100

    End lessons added

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -179,33 +179,33 @@ inherits from the constructor's prototype property.
 
 // Functions returning functions
 
-function interviewQuestion(job) {
-    if (job === 'designer') {
-      return function(name) {
-        console.log(name + ' can you please explain what UX design is ?');
-      }
-    } else if (job === 'teacher') {
-      return function(name) {
-        console.log('What subject do you teach, ' + name + '?');
-      }
-    } else {
-      return function(name) {
-        console.log('Hello' + name + ' , what do you do?');
-      }
-    }
-}
+// function interviewQuestion(job) {
+//     if (job === 'designer') {
+//       return function(name) {
+//         console.log(name + ' can you please explain what UX design is ?');
+//       }
+//     } else if (job === 'teacher') {
+//       return function(name) {
+//         console.log('What subject do you teach, ' + name + '?');
+//       }
+//     } else {
+//       return function(name) {
+//         console.log('Hello' + name + ' , what do you do?');
+//       }
+//     }
+// }
 
 
-var teacherQuestion = interviewQuestion('teacher');
-var designerQuestion = interviewQuestion('designer');
+// var teacherQuestion = interviewQuestion('teacher');
+// var designerQuestion = interviewQuestion('designer');
 
-teacherQuestion('John');
-designerQuestion('John');
-designerQuestion('Jane');
-designerQuestion('Mike');
-designerQuestion('Mark');
+// teacherQuestion('John');
+// designerQuestion('John');
+// designerQuestion('Jane');
+// designerQuestion('Mike');
+// designerQuestion('Mark');
 
-interviewQuestion('teacher')('Mark');
+// interviewQuestion('teacher')('Mark');
 
 
 // function littleBroLuc(job) {
@@ -242,18 +242,143 @@ interviewQuestion('teacher')('Mark');
 
 // game();
 
-(function() {
-  var score = Math.random() * 10;
-    console.log(score >= 5);
-  })();
+// (function() {
+//   var score = Math.random() * 10;
+//     console.log(score >= 5);
+//   })();
 
-// console.log(score);
+// // console.log(score);
 
 
-(function(goodLuck) {
-  var score = Math.random() * 10;
-    console.log(score >= 5 - goodLuck);
-  })(5);
+// (function(goodLuck) {
+//   var score = Math.random() * 10;
+//     console.log(score >= 5 - goodLuck);
+//   })(5);
+
+
+// Closures
+
+/*
+Closure === "An inner function has always access to the variables
+and parameters of its outer function, even after the outer
+function has returned."
+*/
+
+function retirement(retirementAge) {
+  var a = ' years until retirement.';
+  return function(yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+}
+
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIsland = retirement(67);
+
+retirementGermany(1990);
+retirementIsland(1990);
+retirementUS(1990);
+
+
+
+// retirement(66)(1990);
+
+
+// Functions returning functions
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+        console.log(name + ' can you please explain what UX design is ?');
+    } else if (job === 'teacher') {
+        console.log('What subject do you teach, ' + name + '?');
+      } else {
+        console.log('Hello' + name + ' , what do you do?');
+    }
+  }
+}
+
+
+interviewQuestion('teacher')('John');
+
+
+// Bind, call and apply
+
+
+var john = {
+  name: 'John',
+  age: 26,
+  job: 'teacher',
+  presentation: function(style, timeOfDay) {
+    if (style === 'formal') {
+      console.log('Good ' + timeOfDay + ' ladies and gentlemen! I\'m '
+        + this.name + ' , I\'m a ' + this.job + ' and I\'m '
+        + this.age + ' years old.');
+    } else if (style === 'friendly') {
+      console.log('Hey! What\s up? I\'m ' +
+        this.name + ' , I\'m a ' + this.job + ' and I\'m '
+        + this.age + ' years old. Have a nice ' + timeOfDay);
+    }
+  }
+};
+
+
+var emily = {
+  name: 'Emily',
+  age: 35,
+  job: 'designer'
+}
+
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+      arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+
+function isFullAge(limit, el) {
+  return el >= limit;
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+
+console.log(ages);
+console.log(fullJapan);
+
+
+
+
 
 
 
